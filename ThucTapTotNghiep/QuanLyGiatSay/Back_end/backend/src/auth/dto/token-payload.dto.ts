e25d9c83876120fc7b59e167914355e20158c0f1
baouyen53 <baouyen037@gmail.com>
2025-04-25 13:34:29 +0700

    be

--- ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/auth/dto/token-payload.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Role } from '../decorator/role.enum';
-
-export class TokenPayloadDto {
-  _id: string;
-  email: string;
-  name: string;
-  role?: Role[];
-}
--- ThucTapTotNghiep/QuanLyGiatSay/Back_end/backend/src/common/response-pagination.dto.ts
+++
@@ -1,6 +0,0 @@
-export class ResponsePaginationDto {
-  total_items: number;
-  total_pages: number;
-  current_page: number;
-  entites: any;
-}
