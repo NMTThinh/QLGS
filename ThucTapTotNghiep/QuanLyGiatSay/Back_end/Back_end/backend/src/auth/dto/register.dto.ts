b6b86ddde7fe78518b17fb54d98b1a7c4849c4a0
NMTThinh <129759845+NMTThinh@users.noreply.github.com>
2025-05-03 14:28:44 +0700

    Update register.dto.ts

--- ThucTapTotNghiep/QuanLyGiatSay/Back_end/Back_end/backend/src/auth/dto/register.dto.ts
+++ ThucTapTotNghiep/QuanLyGiatSay/Back_end/Back_end/backend/src/auth/dto/register.dto.ts
@@ -10,39 +10,39 @@ import {
   } from 'class-validator';
   import { Role } from '../decorator/role.enum';
   
-  export class RegisterCustomerDto {
-    @IsNotEmpty() // Loại bỏ readonly để có thể gán giá trị
-    @IsString()
-    name: string;
+  // export class RegisterCustomerDto {
+  //   @IsNotEmpty() // Loại bỏ readonly để có thể gán giá trị
+  //   @IsString()
+  //   name: string;
   
-    @IsNotEmpty() // Loại bỏ readonly
-    @IsString()
-    phoneNumber: string;
+  //   @IsNotEmpty() // Loại bỏ readonly
+  //   @IsString()
+  //   phoneNumber: string;
   
-    @IsNotEmpty() // Loại bỏ readonly
-    @IsEmail()
-    email: string;
+  //   @IsNotEmpty() // Loại bỏ readonly
+  //   @IsEmail()
+  //   email: string;
   
-    @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
-    @MinLength(6, { message: 'Mật khẩu phải có ít nhất 6 ký tự' })
-    password: string;
+  //   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
+  //   @MinLength(6, { message: 'Mật khẩu phải có ít nhất 6 ký tự' })
+  //   password: string;
 
-    @IsOptional() // Giữ IsOptional nếu trường có thể không có giá trị
-    @IsString()
-    address: string;
+  //   @IsOptional() // Giữ IsOptional nếu trường có thể không có giá trị
+  //   @IsString()
+  //   address: string;
   
-    @IsOptional() // Giữ IsOptional
-    @IsDateString()
-    birth_date?: Date;
+  //   @IsOptional() // Giữ IsOptional
+  //   @IsDateString()
+  //   birth_date?: Date;
   
-    @IsNotEmpty() // Loại bỏ readonly
-    @IsString()
-    customer_type: string;
+  //   @IsNotEmpty() // Loại bỏ readonly
+  //   @IsString()
+  //   customer_type: string;
   
-    @IsNotEmpty() // Loại bỏ readonly
-    @IsEnum(Role)
-    role: Role;
-  }
+  //   @IsNotEmpty() // Loại bỏ readonly
+  //   @IsEnum(Role)
+  //   role: Role;
+  // }
   export class RegisterStaffDto {
     @IsMongoId()
     @IsNotEmpty()
