5ba92d3d9dfe0b862c74fe4588693a8aae017ee6
NMTThinh <129759845+NMTThinh@users.noreply.github.com>
2025-05-03 14:25:51 +0700

    Update auth.controller.ts

--- ThucTapTotNghiep/QuanLyGiatSay/Back_end/Back_end/backend/src/auth/auth.controller.ts
+++ ThucTapTotNghiep/QuanLyGiatSay/Back_end/Back_end/backend/src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
-import { RegisterCustomerDto, RegisterStaffDto } from './dto/register.dto';
-
+//import { RegisterCustomerDto, RegisterStaffDto } from './dto/register.dto';
+import RegisterStaffDto  from './dto/register.dto';
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -13,20 +13,20 @@ export class AuthController {
   }
 
   // dang nhap customer
-  @Post('customers/login')
-  loginCustomer(@Body() login: LoginDto) {
-    return this.authService.validateCustomer(login);
-  }
+  // @Post('customers/login')
+  // loginCustomer(@Body() login: LoginDto) {
+  //   return this.authService.validateCustomer(login);
+  // }
 
   @Post('staffs/login')
   loginStaff(@Body() login: LoginDto) {
     return this.authService.validateStaff(login);
   }
 
-  @Post('customers/register')
-  registerCustomer(@Body() registerDto: RegisterCustomerDto) {
-    return this.authService.registerCustomer(registerDto);
-  }
+  // @Post('customers/register')
+  // registerCustomer(@Body() registerDto: RegisterCustomerDto) {
+  //   return this.authService.registerCustomer(registerDto);
+  // }
 
   @Post('staffs/register')
   registerStaff(@Body() registerDto: RegisterStaffDto) {
